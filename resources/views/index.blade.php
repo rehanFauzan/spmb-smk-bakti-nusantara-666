f0acf9dd9beb99dc924b254f2183c1aa58117587
rehanFauzan <smkbn666_pk03@LABSA-SMKBN666-PK03s-Mac-mini.local>
2025-11-19 08:08:00 +0700

    update index calon sisiswa2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,7 +20,7 @@
               <div class="floating-card stats-card" data-aos="fade-up" data-aos-delay="400">
                 <div class="stat-item">
                   <span class="number">1500+</span>
-                  <span class="label">Siswa tolol</span>
+                  <span class="label">Siswa Aktif</span>
                 </div>
                 <div class="stat-item">
                   <span class="number">95%</span>
